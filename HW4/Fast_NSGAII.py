531342665658df2efb6a9b91bee315a1c233d112
Atefe Rajabi <atefe.rajabi.78@gmail.com>
2024-01-06 01:26:32 +0330

    Fast NSGAII

--- HW4/Fast_NSGAII.py
+++ HW4/Fast_NSGAII.py
@@ -13,9 +13,12 @@ from pymoo.indicators.igd import IGD
 import time
 import pandas as pd
 from datetime import datetime
+import concurrent.futures
+import multiprocessing
+import logging
 
 class NSGAII:
-    def __init__(self,X,y, population_size, LP, num_features, true_pareto_front, maxFEs, sigma=0.0001):
+    def __init__(self,X,y, population_size, LP, num_features, true_pareto_front,selected_features, maxFEs, sigma=0.0001):
         self.population_size = population_size
         self.LP = LP
         self.mutation_probability= 1 / num_features
@@ -32,12 +35,24 @@ class NSGAII:
         self.X = X
         self.y = y
         self.true_pareto_front = true_pareto_front
+        self.selected_features = selected_features
         
-    def initialize_population(self):
+    def random_initialize_population(self):
         population = []
         for _ in range(self.population_size):
             values = list(np.random.randint(2, size=self.num_features))
             chromosome = Chromosome(values)
+            chromosome.objectives = [f(values) for f in self.objective_functions]
+            population.append(chromosome)
+        return population
+
+    def wise_initialize_population(self):
+        population = []
+        for _ in range(self.population_size):
+            solution = np.random.choice(self.selected_features, size=np.random.randint(1, len(self.selected_features)+1), replace=False)
+            values = np.zeros(self.num_features)
+            values[solution] = 1
+            chromosome = Chromosome(list(values))
             chromosome.objectives = [f(values) for f in self.objective_functions]
             population.append(chromosome)
         return population
@@ -86,6 +101,60 @@ class NSGAII:
                     if q.domination_count == 0:
                         q.rank = i + 1
                         next_front.append(q)
+
+            i += 1
+            fronts.append(next_front)
+        return fronts
+    
+    def environmental_selection_parallel(self, population):
+        remaining_pop_size = self.population_size
+        new_population = []
+        front_0 = []
+        fronts = self.fast_non_dominated_sort_parallel(population)
+        for i, front in enumerate(fronts):
+            front_size = len(front)
+            if remaining_pop_size > front_size:
+                new_population += front
+                remaining_pop_size -= front_size
+                
+            else:
+                self.crowding_distance(front)
+                front.sort(key=lambda chromosome: chromosome.crowding_distance, reverse=True)
+                new_population += front[:remaining_pop_size]
+                
+                break
+        return new_population, fronts[0]
+    
+    def process_individual(self, p, population, fronts):
+        p.domination_count = 0
+        p.dominated_solutions = set()
+        for q in population:
+            if p.dominate(q):
+                p.dominated_solutions.add(q)
+            elif q.dominate(p):
+                p.domination_count += 1
+
+        if p.domination_count == 0:
+            p.rank = 0
+            return p
+        return None
+
+    def fast_non_dominated_sort_parallel(self, population):
+        fronts = [[]]
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(self.process_individual, p, population, fronts) for p in population]
+            concurrent.futures.wait(futures)
+            fronts[0] = [future.result() for future in futures if future.result() is not None]
+        
+        i = 0
+        while fronts[i]:
+            next_front = []
+            for p in fronts[i]:
+                for q in p.dominated_solutions:
+                        q.domination_count -= 1
+                        if q.domination_count == 0:
+                            q.rank = i + 1
+                            next_front.append(q)
 
             i += 1
             fronts.append(next_front)
@@ -179,11 +248,22 @@ class NSGAII:
         ind = IGD(A)
         return ind(pf)
 
+    def calculate_offspring_objectives(self, offspring):
+        offspring.objectives = [f(offspring.values) for f in self.objective_functions]
+        return offspring
     
+
+    def calculate_fitness_parallel(self, offsprings):
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(self.calculate_offspring_objectives, offspring) for offspring in offsprings]
+            concurrent.futures.wait(futures)
+            return [future.result() for future in futures if future.result() is not None]
+       
+
     def nsga2(self):
         count_evaluation = 0
         k = 0
-        population = self.initialize_population()
+        population = self.wise_initialize_population()
         crossover_probability = self.init_OSP()
         hypervolume_values = []
         igd_values = []
@@ -193,8 +273,10 @@ class NSGAII:
             self.nPenalty = {key: self.default_value for key in self.crossover_keys}
             new_population = []
             
-            start_time = time.time()
+#             start_time = time.time()
+            current_families = []
             for _ in range(self.population_size // 2):
+               
                 parent1, parent2 = self.parent_selection(population)
                 rc = Recombination(parent1.values, parent2.values)
                 crossover = rc.roulette_wheel_selection(crossover_probability)
@@ -202,20 +284,24 @@ class NSGAII:
                 offspring1 = self.uniform_mutation(offspring1)
                 offspring2 = self.uniform_mutation(offspring2)
                 
-                offspring1, offspring2 = self.avoid_zero_offspring(offspring1, offspring2)   
-
+                offspring1, offspring2 = self.avoid_zero_offspring(offspring1, offspring2)
                 offspring1, offspring2 = Chromosome(offspring1), Chromosome(offspring2)
-                offspring1.objectives = [f(offspring1.values) for f in self.objective_functions]
-                offspring2.objectives = [f(offspring2.values) for f in self.objective_functions]
-                
-
-                count_evaluation += 2
-                self.credit_assignment([parent1, parent2], [offspring1, offspring2], crossover)
+                # offsprings = self.calculate_fitness_parallel([offspring1, offspring2])
+                current_families.append([[offspring1, offspring2],[parent1, parent2],crossover])
                 new_population += [offspring1, offspring2]
-                
-            end_time = time.time()
-            elapsed_time = end_time - start_time
-            print("new_pop time: ", elapsed_time)
+              
+
+            offsprings = self.calculate_fitness_parallel(new_population)
+            count_evaluation += 100
+            offsprings = np.array(offsprings).reshape(len(current_families),2).tolist()
+            for i, family_offsprings in enumerate(offsprings):
+                current_families[i][0] = family_offsprings
+                self.credit_assignment(current_families[i][1], family_offsprings, current_families[i][2])
+
+#             end_time = time.time()
+#             elapsed_time = end_time - start_time
+#             print("new_pop time: ", elapsed_time)
+
             k += 1
             self.RD.append(self.nReward)
             self.PN.append(self.nPenalty)
@@ -228,25 +314,28 @@ class NSGAII:
             
             distinct_objects_set = set()
             distinct_objects = []
+
             for obj in current_pool:
                 obj_values_tuple = tuple(obj.values)
                 if obj_values_tuple not in distinct_objects_set:
                     distinct_objects_set.add(obj_values_tuple)
                     distinct_objects.append(obj)
+                
+            
 
-            start_time = time.time()
-            population, current_solutions = self.environmental_selection(distinct_objects)
-#           current_solutions = self.find_non_dominated_solution(population)
-            end_time = time.time()
-            elapsed_time = end_time - start_time
-            print("environmental: ", elapsed_time)
+#             start_time = time.time()
+            population, current_solutions = self.environmental_selection_parallel(distinct_objects)
+#             current_solutions = self.find_non_dominated_solution(population)
+#             end_time = time.time()
+#             elapsed_time = end_time - start_time
+#             print("environmental time: ", elapsed_time)
             
             hypervolume = self.calculate_hypervolume(current_solutions)
             hypervolume_values.append(hypervolume)
             igd = self.calculate_igd(current_solutions)
             igd_values.append(igd)
             
-            if count_evaluation % 100000 == 0:
+            if count_evaluation % 25000 == 0:
                 print(f"Evaluations: {count_evaluation}, HV: {hypervolume}, IGD: {igd}")
 
 
